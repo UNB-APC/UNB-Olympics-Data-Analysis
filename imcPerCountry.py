c6895a2863c83b737b6a5622d1b290396bf2bae6
CauaMatheus <caua10000@gmail.com>
2021-09-24 22:50:44 -0300

    feat: adicionando card de paises nao representados no grafico

--- imcPerCountry.py
+++ imcPerCountry.py
@@ -1,5 +1,4 @@
 from math import isnan
-from functools import reduce
 from dash.dependencies import Input, Output
 
 from pandas import read_csv
@@ -38,17 +37,18 @@ for cells in csvDataFrame.values:
     continue
 
   year = cells[9] # Ano
-  team = cells[7] # Time
+  noc = cells[7] # ISO Country Value
+  team = cells[6] # Team Name
   height = cells[4]/100 # Centimeters to meters
   weight = cells[5] # Peso
   imc = weight/(height**2)
 
   if groupedByYear.get(year) == None:
     groupedByYear[year] = {}
-  if groupedByYear[year].get(team) == None:
-    groupedByYear[year][team] = []
+  if groupedByYear[year].get(noc) == None:
+    groupedByYear[year][noc] = []
 
-  groupedByYear[year][team].append(imc)
+  groupedByYear[year][noc].append({"imc": imc, "team": team})
 # ------------------------------------------------------------
 
 # List All Years
@@ -60,14 +60,18 @@ years.sort()
 
 # Create The Main DataFrame
 # ------------------------------------------------------------
-dataFrameObject = {'team':[], 'year':[], 'imcAverage': []}
+dataFrameObject = {'team':[], 'year':[], 'imcAverage': [], 'noc': []}
 for year in years:
-  for team in groupedByYear[year]:
-    imcSum = reduce(lambda acc, current: acc+current, groupedByYear[year][team])
-    imcAverage = imcSum / len(groupedByYear[year][team])
+  for noc in groupedByYear[year]:
+    imcSum = 0
+    for data in groupedByYear[year][noc]:
+      imcSum += data["imc"]
+
+    imcAverage = imcSum / len(groupedByYear[year][noc])
 
     dataFrameObject['year'].append(year)
-    dataFrameObject['team'].append(team)
+    dataFrameObject['noc'].append(noc)
+    dataFrameObject['team'].append(groupedByYear[year][noc][0]["team"])
     dataFrameObject['imcAverage'].append(imcAverage)
 # ------------------------------------------------------------
 
@@ -108,10 +112,40 @@ app.layout = html.Div(
               style={'width': '100px'},
             ),
 
-            dcc.Graph(
-              id="graph",
-              figure=[],
-            ),
+            html.Div(
+              className="graph-container",
+              children=[
+                dcc.Graph(
+                  id="graph",
+                  figure=[],
+                ),
+
+                html.Div(
+                  className="notListedTeams",
+                  children=[
+                    html.H1("Times Não Listados"),
+                    html.Table(
+                      children=[
+                        html.Thead(
+                          children=[
+                            html.Tr(
+                              children=[
+                                html.Th("Time"),
+                                html.Th("Média do IMC")
+                              ]
+                            )
+                          ]
+                        ),
+                        html.Tbody(
+                          id="itemsTable",
+                          children=[]
+                        )
+                      ]
+                    )
+                  ]
+                ),
+              ]
+            )
           ],
         ),
       ]
@@ -124,22 +158,24 @@ colors = [
   "#00A651",
 ]
 
+ISOCountries = ["ABW","AFG","AGO","AIA","ALA","ALB","AND","ARE","ARG","ARM","ASM","ATA","ATF","ATG","AUS","AUT","AZE","BDI","BEL","BEN","BES","BFA","BGD","BGR","BHR","BHS","BIH","BLM","BLR","BLZ","BMU","BOL","BRA","BRB","BRN","BTN","BVT","BWA","CAF","CAN","CCK","CHE","CHL","CHN","CIV","CMR","COD","COG","COK","COL","COM","CPV","CRI","CUB","CUW","CXR","CYM","CYP","CZE","DEU","DJI","DMA","DNK","DOM","DZA","ECU","EGY","ERI","ESH","ESP","EST","ETH","FIN","FJI","FLK","FRA","FRO","FSM","GAB","GBR","GEO","GGY","GHA","GIB","GIN","GLP","GMB","GNB","GNQ","GRC","GRD","GRL","GTM","GUF","GUM","GUY","HKG","HMD","HND","HRV","HTI","HUN","IDN","IMN","IND","IOT","IRL","IRN","IRQ","ISL","ISR","ITA","JAM","JEY","JOR","JPN","KAZ","KEN","KGZ","KHM","KIR","KNA","KOR","KWT","LAO","LBN","LBR","LBY","LCA","LIE","LKA","LSO","LTU","LUX","LVA","MAC","MAF","MAR","MCO","MDA","MDG","MDV","MEX","MHL","MKD","MLI","MLT","MMR","MNE","MNG","MNP","MOZ","MRT","MSR","MTQ","MUS","MWI","MYS","MYT","NAM","NCL","NER","NFK","NGA","NIC","NIU","NLD","NOR","NPL","NRU","NZL","OMN","PAK","PAN","PCN","PER","PHL","PLW","PNG","POL","PRI","PRK","PRT","PRY","PSE","PYF","QAT","REU","ROU","RUS","RWA","SAU","SDN","SEN","SGP","SGS","SHN","SJM","SLB","SLE","SLV","SMR","SOM","SPM","SRB","SSD","STP","SUR","SVK","SVN","SWE","SWZ","SXM","SYC","SYR","TCA","TCD","TGO","THA","TJK","TKL","TKM","TLS","TON","TTO","TUN","TUR","TUV","TWN","TZA","UGA","UKR","UMI","URY","USA","UZB","VAT","VCT","VEN","VGB","VIR","VNM","VUT","WLF","WSM","YEM","ZAF","ZMB","ZWE"]
 @app.callback(
-  Output(component_id="graph", component_property="figure"),
+  [Output(component_id="graph", component_property="figure"), Output(component_id="itemsTable", component_property="children")],
   [Input(component_id="selectedYear", component_property="value")]
 )
 def updateGraph(selectedYear):
   copy = dataFrameObject.copy()
-  df = {"team":[], "imcAverage": []}
+  df = {"team":[], "imcAverage": [], "noc": []}
 
   for index in range(len(copy["year"])):
     if copy["year"][index] == selectedYear:
-      df["team"].append(copy["team"][index])
+      df["noc"].append(copy["noc"][index])
       df["imcAverage"].append(copy["imcAverage"][index])
+      df["team"].append(copy["team"][index])
 
   figure = px.choropleth(
     df,
-    locations="team",
+    locations="noc",
     color="imcAverage",
     hover_name="team",
     color_continuous_scale=colors,
@@ -151,7 +187,21 @@ def updateGraph(selectedYear):
   layout["paper_bgcolor"] = "#F7F8FA"
   layout["plot_bgcolor"] = "#F7F8FA"
 
-  return figure
+  notListedTeams = []
+  for index in range(len(df["team"])):
+    if df["noc"][index] not in ISOCountries:
+      notListedTeams.append({"team": df["team"][index], "imcAverage": df["imcAverage"][index]})
+
+  TrForEachNotListedTeam = [
+    html.Tr(
+      children=[
+        html.Td(item["team"]),
+        html.Td(f'{item["imcAverage"]:.2f}')
+      ]
+    ) for item in notListedTeams
+  ]
+
+  return [figure, TrForEachNotListedTeam]
 #------------------------------------------------------------
 
 
