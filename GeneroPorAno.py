463118c6a6af1792eb00274cac092dc5ec2dda09
rabelo21 <eric.rabelo21@gmail.com>
2021-10-04 18:31:28 -0300

    comentando o código

--- GeneroPorAno.py
+++ GeneroPorAno.py
@@ -1,11 +1,13 @@
-from pandas import read_csv #importação do leitor de dados
+from pandas import read_csv 
+#importação do leitor de dados
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 from dash import Dash, html, dcc
 
-dados = read_csv("dados/athlete_events.csv", delimiter = ",")
+dados = read_csv("dados/athlete_events.csv", delimiter = ",") 
+#lendo de dados do arquivo csv
 
-agruparporano = {}
+agrupadosporano = {}
 
 '''
 0  'ID',
@@ -26,34 +28,40 @@ agruparporano = {}
 '''
 
 idsLidos = set()
-for linha in dados.values: #atribuir sexo aos anos 
+
+for linha in dados.values: 
     if linha[0] in idsLidos:
         continue
+    #condicional para que os ids já armazenados no set não sejam lidos novamente
 
     ano = linha[9]
     temporada = linha[10]
     sexo = linha[2]
-    if agruparporano.get(ano) == None:
-        agruparporano[ano] = {"Summer": [], "Winter": []}
+    if agrupadosporano.get(ano) == None:
+    #condicional para verificar se o ano já existe dentro da dict "agrupadosporano"
+        agrupadosporano[ano] = {"Summer": [], "Winter": []}
+    
         
-    agruparporano[ano][temporada].append(sexo)
+    agrupadosporano[ano][temporada].append(sexo)
     idsLidos.add(linha[0])
 
 
-anos = list(agruparporano.keys())
+anos = list(agrupadosporano.keys())
 anos.sort()
-
+#ordenando os anos dentro da list em ordem crescente
 
 seasonsDataFrame = {
     "Summer": {"Ano":[],"Homens":[], "Mulheres":[]},
     "Winter": {"Ano":[],"Homens":[], "Mulheres":[]}
 }
+#criando dicionário para armazenar os valores de "Ano", "Homens" e "Mulheres" dentro das keys: Summer e Winter
+
 for ano in anos:
-                  # agruparporano[ano] = {"Summer": [], "Winter": []}
-    for season in agruparporano[ano]: # season pode ser Summer ou Winter
+    for season in agrupadosporano[ano]: #
+        #season pode ser Summer ou Winter
         male = 0
         female = 0
-        for sexo in agruparporano[ano][season]:
+        for sexo in agrupadosporano[ano][season]:
             if sexo == "M":
                 male += 1 
                 continue 
@@ -61,12 +69,16 @@ for ano in anos:
 
         if male == 0 and female == 0:
             continue
+        
         seasonsDataFrame[season]["Ano"].append(ano)
         seasonsDataFrame[season]["Homens"].append(male)
         seasonsDataFrame[season]["Mulheres"].append(female)
+        #armazenando o ano e a quantidade de homens e mulheres (a cada ano) nos dataframes 
+
         
 
 app = Dash(__name__)
+#iniciando o dash
 
 app.layout = html.Div(
     className="container-imc-per-country",
@@ -88,7 +100,8 @@ app.layout = html.Div(
                 html.H1("Gênero por Ano", className="title"),
                 html.Section(
                     [
-                        dcc.Dropdown(
+                        dcc.Dropdown( 
+                        #menu suspenso para alternar o gráfico entre as temporadas summer e winter
                             id="selectedSeason",
                             options=[{"label": "Inverno", "value":"Winter"}, {"label": "Verão", "value": "Summer"}],
                             value= "Summer",
@@ -104,12 +117,17 @@ app.layout = html.Div(
         ),
     ],
 )
+#layout do dash 
 
 @app.callback(
     Output(component_id="graph", component_property="figure"),
     [Input(component_id="selectedSeason", component_property="value")],
 )
-def updateGraph(selectedSeason): # selectedSeason pode ser Winter ou Summer
+#callback para atualizar o gráfico de acordo com o input do usuário 
+
+def updateGraph(selectedSeason): 
+#função para atualizar o gráfico
+
     df = seasonsDataFrame[selectedSeason]
 
     grafico = go.Figure()
@@ -117,5 +135,7 @@ def updateGraph(selectedSeason): # selectedSeason pode ser Winter ou Summer
     grafico.add_trace(go.Scatter(x=df["Ano"], y=df["Mulheres"], name = "Mulheres"))
     grafico.update_layout(width=1200, height=675)
     return grafico
+
 app.run_server()
+#rodando o gráfico no dash
     
